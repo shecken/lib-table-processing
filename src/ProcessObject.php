96a21d409af074678cdfd1cb65fd603ec7919e67
daniwe4 <daniel.weise@concepts-and-training.de>
2018-08-15 14:42:18 +0200

    add an interface each process object should implement

---
+++ src/ProcessObject.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CaT\Libs\TableProcessing;
+
+/**
+ * Interface
+ *
+ * @author Daniel Weise <daniel.weise@concepts-and-training.de>
+ */
+interface ProcessObject
+{
+	public function getId(): int;
+}
